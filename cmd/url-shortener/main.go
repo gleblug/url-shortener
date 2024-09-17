57ff09051f36f004723980733e4871f3c692aac6
Lugovtsov Gleb <galera951@gmail.com>
2024-09-17 18:56:31 +0300

    Move redirect handler

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/http-server/handlers/url/redirect"
 	"url-shortener/internal/http-server/handlers/url/save"
 	mwLogger "url-shortener/internal/http-server/middleware/logger"
 	"url-shortener/internal/lib/logger/handlers/slogpretty"
@@ -46,7 +47,8 @@ func main() {
 	router.Use(middleware.URLFormat)
 
 	router.Post("/url", save.New(log, storage))
-	router.Get("/u/{alias}", redirect.New(log, storage))
+	router.Get("/{alias}", redirect.New(log, storage))
+	// TODO: add .Delete
 
 	log.Info("starting server", slog.String("address", cfg.Address))
 
