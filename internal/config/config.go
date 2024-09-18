a46c5067d6c0f8ae5696294abcf93b0ae685e75a
Gleb Lugovtsov <galera951@gmail.com>
2024-09-18 14:20:53 +0300

    Fix typo

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,7 +11,7 @@ import (
 
 type Config struct {
 	Env         string `yaml:"env" env-default:"prod"` // ENV: "", env-required: ""
-	StoragePath string `yaml:"storage_pass" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
 	HTTPServer  `yaml:"http_server"`
 }
 
